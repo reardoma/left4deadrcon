75292f522683d7081e40b7ff8861a49b68b4a91b
dave.cranwell <dave.cranwell@a60181e2-ba14-11dd-aa87-4b319d2fb4de>
2008-11-24 12:17:47 +0000

    added contact

--- trunk/display_includes/bottom.php
+++ trunk/display_includes/bottom.php
@@ -1,4 +1,4 @@
-<div id="footer">Host your own version of this web application by <a href="http://code.google.com/p/left4deadrcon/">downloading the source here</a></div>
+<div id="footer">Host your own version of this web application by <a href="http://code.google.com/p/left4deadrcon/">downloading the source here</a> | <a href="l4drcon@evil-leet.com">Questions/Suggestions/Comments</a></div>
 
 </body>
 </head>
