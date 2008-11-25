330df0f0774a876df7d477269d3ce498109c167d
dave.cranwell <dave.cranwell@a60181e2-ba14-11dd-aa87-4b319d2fb4de>
2008-11-25 00:16:55 +0000

    removed testing data

--- trunk/rpc/serverstatus.php
+++ trunk/rpc/serverstatus.php
@@ -6,14 +6,7 @@
 		$rcon= new RCon($_SESSION['server'], $_SESSION['port'], $_SESSION['password']);
 		$rcon ->authenticate();
 		$status=$rcon->getStatus();
-		
-		$playerInfo = explode(" ", $status['players']);
-		$playerCount = $playerInfo[0];
-		
-		//REMOVE ME
-		$playerCount=1;
-		
-				
+								
 		echo '({
 			"ip": '.json_encode($status['ip']).',
 			"hostname": '.json_encode($status['hostname']).',
@@ -21,7 +14,7 @@
 			"difficulty": '.json_encode($status['difficulty']).',
 			"players": '.json_encode($status['players']).',
 			"playerinfo": [';
-				for($i = 1; $i <= $playerCount; $i++){
+				for($i = 1; $i <= $status['playercount']; $i++){
 					echo '{
 						"id": '.json_encode($status['player'.$i]['id']).',
 						"name": '.json_encode($status['player'.$i]["name"]).',
